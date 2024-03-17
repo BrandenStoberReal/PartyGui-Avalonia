eeb901e769a8b37d51d2c52d086c567035591ae0
Branden Stober <brandenstober@outlook.com>
2024-03-17 18:24:33 -0400

    Initial Push

---
+++ PartyGui_Avalonia_New/PartyGui_Avalonia_New/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PartyGui_Avalonia_New.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
